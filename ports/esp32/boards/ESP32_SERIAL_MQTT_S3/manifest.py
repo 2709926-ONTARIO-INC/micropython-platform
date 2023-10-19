1f3fcca3fbbeda8dace207d245e4f64e90784f12
sramrajkar <55571093+sramrajkar@users.noreply.github.com>
2023-10-18 22:10:46 -0400

    WiFi connects and base64 functional

--- ports/esp32/boards/ESP32_SERIAL_MQTT_S3/manifest.py
+++ ports/esp32/boards/ESP32_SERIAL_MQTT_S3/manifest.py
@@ -1,2 +1,12 @@
-include("$(PORT_DIR)/boards/manifest.py")
+freeze("$(PORT_DIR)/modules")
 freeze("modules")
+include("$(MPY_DIR)/extmod/asyncio")
+
+# Useful networking-related packages.
+require("bundle-networking")
+
+# Require some micropython-lib modules.
+require("base64")
+require("umqtt.robust")
+require("umqtt.simple")
+
--- ports/esp32/boards/ESP32_SERIAL_MQTT_S3/modules/serial_mqtt.py
+++ ports/esp32/boards/ESP32_SERIAL_MQTT_S3/modules/serial_mqtt.py
@@ -5,27 +5,33 @@
 
 # Import required libraries
 import uasyncio as asyncio
+from base64 import b64encode, b64decode
 from machine import UART
+from wifi_mgr import wifi_connect
 
-uart = UART(1, 9600, tx=16, rx=15, rts=17, mode=UART.MODE_RS485, timeout=0, timeout_char=10)
+uart = UART(1, 9600, tx=16, rx=15, rts=17, mode=UART.MODE_RS485, timeout=10, timeout_char=10)
+ser_to_mqtt = list()
 
 async def sender():
     swriter = asyncio.StreamWriter(uart, {})
     while True:
-        swriter.write('Hello uart\n')
-        await swriter.drain()
+        if(len(ser_to_mqtt) > 0):
+            swriter.write(b64decode(ser_to_mqtt.pop(0)))
+            await swriter.drain()
         await asyncio.sleep(2)
 
 async def receiver():
     sreader = asyncio.StreamReader(uart)
     while True:
-        res = await sreader.read()
+        res = await sreader.read(1024)
         if(None != res):
             print('Received', res)
+            ser_to_mqtt.append(b64encode(res))
 
 async def main():
     asyncio.create_task(sender())
     asyncio.create_task(receiver())
+    asyncio.create_task(wifi_connect())
     while True:
         await asyncio.sleep(1)
 
