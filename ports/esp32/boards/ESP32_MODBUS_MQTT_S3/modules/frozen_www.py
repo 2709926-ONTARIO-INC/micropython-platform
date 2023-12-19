9b0c04f9f147765b3c0d1982a0a0472cc996f37f
sramrajkar <55571093+sramrajkar@users.noreply.github.com>
2023-12-19 10:30:06 -0500

    index page served from ESP32 over ethernet

---
+++ ports/esp32/boards/ESP32_MODBUS_MQTT_S3/modules/frozen_www.py
@@ -0,0 +1,420 @@
+# /index.html size=4510
+_f0 = const(
+  b'<!DOCTYPE html>\r'\
+  b'\n<html lang="en"'\
+  b'>\r\n<head>\r\n    <'\
+  b'meta charset="UT'\
+  b'F-8">\r\n    <meta'\
+  b' name="viewport"'\
+  b' content="width='\
+  b'device-width, in'\
+  b'itial-scale=1.0"'\
+  b'>\r\n    <title>Si'\
+  b'gn In</title>\r\n '\
+  b'   <script src="'\
+  b'https://kit.font'\
+  b'awesome.com/374f'\
+  b'8eef92.js" cross'\
+  b'origin="anonymou'\
+  b's"></script>\r\n\r\n'\
+  b'    <style>\r\n   '\
+  b'     @import url'\
+  b"('https://fonts."\
+  b'googleapis.com/c'\
+  b'ss2?family=Nunit'\
+  b'o+Sans:opsz@6..1'\
+  b'2&family=Poppins'\
+  b"&display=swap');"\
+  b'\r\n        * {\r\n '\
+  b'           margi'\
+  b'n: 0;\r\n         '\
+  b'   padding: 0;\r\n'\
+  b'            font'\
+  b"-family: 'Poppin"\
+  b"s', sans-serif;\r"\
+  b'\n            box'\
+  b'-sizing: border-'\
+  b'box;\r\n        }\r'\
+  b'\n\r\n        .cont'\
+  b'ainer{\r\n        '\
+  b'    width: 100%;'\
+  b'\r\n            he'\
+  b'ight: 100vh;\r\n  '\
+  b'          backgr'\
+  b'ound-color: #FFF'\
+  b'2D8;\r\n        }\r'\
+  b'\n\r\n        .form'\
+  b'-box{\r\n         '\
+  b'   width: 90%;\r\n'\
+  b'            max-'\
+  b'width: 450px;\r\n '\
+  b'           posit'\
+  b'ion: absolute;\r\n'\
+  b'            top:'\
+  b' 50%;\r\n         '\
+  b'   left: 50%;\r\n '\
+  b'           trans'\
+  b'form: translate('\
+  b'-50%,-50%);\r\n   '\
+  b'         backgro'\
+  b'und: #AE445A;\r\n '\
+  b'           paddi'\
+  b'ng: 50px 60px 70'\
+  b'px;\r\n           '\
+  b' text-align: cen'\
+  b'ter;\r\n        }\r'\
+  b'\n\r\n        .form'\
+  b'-box h1{\r\n      '\
+  b'      font-size:'\
+  b' 30px;\r\n        '\
+  b'    margin-botto'\
+  b'm: 60px;\r\n      '\
+  b'      color: #FF'\
+  b'F2D8;\r\n         '\
+  b'   position: rel'\
+  b'ative;\r\n        '\
+  b'}\r\n\r\n        .fo'\
+  b'rm-box h1::after'\
+  b'{\r\n            c'\
+  b"ontent: '';\r\n   "\
+  b'         width: '\
+  b'30px;\r\n         '\
+  b'   height: 4px;\r'\
+  b'\n            bor'\
+  b'der-radius: 3px;'\
+  b'\r\n            ba'\
+  b'ckground: #FFF2D'\
+  b'8;\r\n            '\
+  b'position: absolu'\
+  b'te;\r\n           '\
+  b' bottom: -12px;\r'\
+  b'\n            lef'\
+  b't: 50%;\r\n       '\
+  b'     transform: '\
+  b'translateX(-50%)'\
+  b';\r\n        }\r\n\r\n'\
+  b'        .input-f'\
+  b'ield{\r\n         '\
+  b'   background: #'\
+  b'FFF2D8;\r\n       '\
+  b'     margin: 15p'\
+  b'x 0;\r\n          '\
+  b'  border-radius:'\
+  b' 3px;\r\n         '\
+  b'   display: flex'\
+  b';\r\n            a'\
+  b'lign-items: cent'\
+  b'er;\r\n           '\
+  b' max-height: 65p'\
+  b'x;\r\n            '\
+  b'transition: max-'\
+  b'height 0.5s;\r\n  '\
+  b'          overfl'\
+  b'ow: hidden;\r\n   '\
+  b'     }\r\n        '\
+  b'input{\r\n        '\
+  b'    width: 100%;'\
+  b'\r\n            ba'\
+  b'ckground: transp'\
+  b'arent;\r\n        '\
+  b'    border: 0;\r\n'\
+  b'            outl'\
+  b'ine: 0;\r\n       '\
+  b'     padding: 18'\
+  b'px 15px;\r\n      '\
+  b'  }\r\n        .in'\
+  b'put-field i{\r\n  '\
+  b'          margin'\
+  b'-left: 15px;\r\n  '\
+  b'      }\r\n       '\
+  b' form p{\r\n      '\
+  b'      text-align'\
+  b': center;\r\n     '\
+  b'       font-size'\
+  b': 13px;\r\n       '\
+  b'     color: #FFF'\
+  b'2D8;\r\n        }\r'\
+  b'\n        form p '\
+  b'a{\r\n            '\
+  b'text-decoration:'\
+  b' none;\r\n        '\
+  b'    color: #fff;'\
+  b'\r\n        }\r\n\r\n '\
+  b'       .btn-fiel'\
+  b'd{\r\n            '\
+  b'width: 100%;\r\n  '\
+  b'          displa'\
+  b'y: flex;\r\n      '\
+  b'      justify-co'\
+  b'ntent: space-bet'\
+  b'ween;\r\n        }'\
+  b'\r\n\r\n        .btn'\
+  b'-field button{\r\n'\
+  b'            flex'\
+  b'-basis: 48%;\r\n  '\
+  b'          backgr'\
+  b'ound: #FFF2D8;\r\n'\
+  b'            heig'\
+  b'ht: 40px;\r\n     '\
+  b'       border-ra'\
+  b'dius: 10px;\r\n   '\
+  b'         border:'\
+  b' 0;\r\n           '\
+  b' outline: 0;\r\n  '\
+  b'          cursor'\
+  b': pointer;\r\n    '\
+  b'        transiti'\
+  b'on: background 1'\
+  b's;\r\n            '\
+  b'margin-top: 20px'\
+  b';\r\n        }\r\n\r\n'\
+  b'        .btn-fie'\
+  b'ld button.disabl'\
+  b'e{\r\n            '\
+  b'background: #ffc'\
+  b'ad9;\r\n        }\r'\
+  b'\n\r\n        .subm'\
+  b'it{\r\n           '\
+  b' justify-content'\
+  b': center;\r\n     '\
+  b'   }\r\n        .s'\
+  b'ubmit-btn{\r\n    '\
+  b'        justify-'\
+  b'content: center;'\
+  b'\r\n            ma'\
+  b'rgin-bottom: 6px'\
+  b';\r\n            m'\
+  b'argin-top: -12px'\
+  b';\r\n        }\r\n  '\
+  b'  </style>\r\n</he'\
+  b'ad>\r\n<body>\r\n   '\
+  b' <div class="con'\
+  b'tainer">\r\n      '\
+  b'  <div class="fo'\
+  b'rm-box">\r\n      '\
+  b'      <h1 id="ti'\
+  b'tle">Sign In</h1'\
+  b'>\r\n            <'\
+  b'form>\r\n         '\
+  b'       <div clas'\
+  b's="input-group">'\
+  b'\r\n              '\
+  b'      <div class'\
+  b'="input-field" >'\
+  b'\r\n              '\
+  b'          <i cla'\
+  b'ss="fa-solid fa-'\
+  b'user"></i>\r\n    '\
+  b'                '\
+  b'    <input type='\
+  b'"email" id="user'\
+  b'name" placeholde'\
+  b'r="Username">\r\n '\
+  b'                '\
+  b'   </div>\r\n     '\
+  b'               <'\
+  b'div class="input'\
+  b'-field" >\r\n     '\
+  b'                '\
+  b'   <i class="fa-'\
+  b'solid fa-lock"><'\
+  b'/i>\r\n           '\
+  b'             <in'\
+  b'put type="passwo'\
+  b'rd" id="password'\
+  b'" placeholder="P'\
+  b'assword">\r\n     '\
+  b'               <'\
+  b'/div>\r\n         '\
+  b'           <p>Fo'\
+  b'rgot Password?<a'\
+  b' href="#"> Click'\
+  b' here!</a></p>\r\n'\
+  b'                '\
+  b'    <div class="'\
+  b'btn-field submit'\
+  b'-btn">\r\n        '\
+  b'                '\
+  b'<button type="bu'\
+  b'tton" onclick="m'\
+  b'yFunction()" cla'\
+  b'ss="submit">Subm'\
+  b'it</button>     '\
+  b'\r\n              '\
+  b'      </div>\r\n  '\
+  b'                '\
+  b'  \r\n            '\
+  b'    </div>\r\n    '\
+  b'            \r\n  '\
+  b'          </form'\
+  b'>\r\n        </div'\
+  b'>\r\n    </div>\r\n '\
+  b'   <script>\r\n   '\
+  b'     \r\n        f'\
+  b'unction myFuncti'\
+  b'on() {\r\n        '\
+  b'    const login_'\
+  b'credentials = {\r'\
+  b'\n               '\
+  b' "username": doc'\
+  b'ument.getElement'\
+  b'ById("username")'\
+  b'.value,\r\n       '\
+  b'         "passwo'\
+  b'rd": document.ge'\
+  b'tElementById("pa'\
+  b'ssword").value,\r'\
+  b'\n            };\r'\
+  b'\n\r\n            v'\
+  b'ar json_format ='\
+  b' JSON.stringify('\
+  b'login_credential'\
+  b's);\r\n           '\
+  b' console.log(jso'\
+  b'n_format);\r\n\r\n  '\
+  b'          const '\
+  b'parsed_json = JS'\
+  b'ON.parse(json_fo'\
+  b'rmat);\r\n        '\
+  b'    console.log('\
+  b'parsed_json.user'\
+  b'name);\r\n        '\
+  b'}\r\n\r\n        \r\n '\
+  b'   </script>\r\n</'\
+  b'body>\r\n</html>'\
+  b'')
+direntries = const(( ( '/index.html',  ( _f0, False, 4510 ) ),
+))
+
+version = const(2)
+date_frozen = const( '2023/12/19 10:17:11' )
+
+sum_size = const(4510)
+files_folders = const(1)
+import os
+import errno
+from io import BytesIO, StringIO
+from collections import OrderedDict
+import sys
+def _get_basename( filename ):
+ return filename.split("/")[-1]
+def _get_folder( filename ):
+ basename = _get_basename( filename )
+ folder = filename[0:-len(basename)-1]
+ if folder == "":
+  folder = "/"
+ return folder
+class VfsFrozen:
+ def __init__( self, direntries, sum_size, files_folders ):    
+  self.filedict = OrderedDict( direntries )
+  self.sum_size = sum_size
+  self.files_folders = files_folders
+  self.path = "/"
+ def _to_absolute_filename( self, filename ):
+  if not filename.startswith( "/" ) :
+   filename = self.path + "/" + filename
+  if filename.endswith("/") and filename != "/":
+   filename = filename[0:-1]
+  filename = filename.replace("//", "/")
+  parts = filename.split("/")
+  i = -1
+  while ".." in parts:
+   i = parts.index("..")
+   if i > 1:
+    del parts[i]
+    del parts[i-1]
+   else:
+    raise OSError( errno.EPERM )
+  while "." in parts:
+   i = parts.index(".")
+   del parts[i]
+  filename = "".join( "/" + p for p in parts ).replace( "//", "/" )
+  return filename
+ def _find_file( self, filename ):
+  filename = self._to_absolute_filename( filename )
+  if filename == "/":
+   return None
+  if filename in self.filedict:
+   return self.filedict[filename]
+  else:
+   raise OSError( errno.ENOENT )
+ def open( self, filename, mode, buffering=None ):
+  for c in mode:
+   if c not in "rbt":
+    raise OSError( errno.EINVAL )
+  dir_entry = self._find_file( filename )
+  if dir_entry is None: 
+   if filename == "/":
+    raise OSError( errno.EPERM )
+   raise OSError(errno.EISDIR)
+  data = dir_entry[0] # data 
+  if not dir_entry[1]: # compressed
+   if "b" in mode:
+    return BytesIO( data )
+   else:
+    return StringIO( data )
+  else:
+   from deflate import DeflateIO, AUTO
+   uncompressed_stream = DeflateIO( BytesIO( data ),
+       AUTO, 0, True )
+   if "b" in mode:
+    return uncompressed_stream
+   else:
+    return StringIO( uncompressed_stream.read() )
+ def chdir( self, path ):
+  newdir = self._to_absolute_filename( path )
+  dir_entry = self._find_file( newdir )
+  if dir_entry is None:
+   self.path = newdir
+   return
+  raise OSError( -2 )
+ def getcwd( self ):
+  if self.path != "/" and self.path.endswith( "/" ):
+   return self.path[0:-1]
+  return self.path
+ def ilistdir( self, path ):
+  abspath = self._to_absolute_filename( path ) 
+  self._find_file( abspath )
+  for filename, dir_entry in self.filedict.items():
+   if _get_folder( filename ) == abspath:
+    basename = _get_basename( filename )
+    if dir_entry is not None:
+     yield ( basename, 0x8000, 0,  dir_entry[2] )
+    else:
+     yield ( basename, 0x4000, 0, 0 )
+ def stat( self, filename ): 
+  dir_entry = self._find_file( filename )
+  if dir_entry is None:
+   return (0x4000, 0,0,0,0,0, 0, 0,0,0)
+  else:
+   return (0x8000, 0,0,0,0,0, dir_entry[2], 0,0,0)
+ def statvfs( self, *args ):
+  return (1,1,self.sum_size,  0,0,self.files_folders,  0,0,1, 255)
+ def mount( self, readonly, x ):
+  self.path = "/"
+ def remove( self, filename ):
+  raise OSError( errno.EPERM )
+ def mkdir( self, *args ):
+  raise OSError( errno.EPERM )
+ def rename( self, oldfname, newfname ):
+  raise OSError( errno.EPERM )
+ def umount( self ):
+  pass
+def mount_fs( frozen_module_name, target, silent ):
+ module = __import__( frozen_module_name )
+ if target is None:
+  raise ValueError("No target specified")
+ file_exists = False
+ try:
+  os.stat( target )
+  file_exists = True
+ except:
+  pass  
+ if file_exists:
+  raise OSError( errno.EEXIST )
+ if not silent:
+  print( f"mounting {__name__} at {target}." )
+ os.mount( VfsFrozen( module.direntries, module.sum_size, module.files_folders ), target, readonly=True )
+ return True
+
+mount_fs( __name__, '/www', False )
